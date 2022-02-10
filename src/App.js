06b7cf34cc30287c0885720846917f6066555883
gill327 <95605742+gill327@users.noreply.github.com>
2022-02-10 16:32:42 -0600

    react

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './goose.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          gill327's site is currently under construction<br/>
+          please check back soon.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
