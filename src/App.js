e7d0623c162a991f3ef55fa9f4766dd22679c00e
gill327 <95605742+gill327@users.noreply.github.com>
2022-02-11 01:53:52 -0600

    added construction animation

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import logo from './goose.svg';
+import water from './water.svg';
 import './App.css';
 
 function App() {
@@ -6,7 +7,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <img src={water} className="water left" alt="There's water here, trust me." />
+        <p className="floating">
           gill327's site is currently under construction<br/>
           please check back soon.
         </p>
