4fa1b4965d977a85f65b8912fce4c5bf82336719
gill327 <95605742+gill327@users.noreply.github.com>
2022-06-28 16:52:16 -0500

    worked on adaptive nav bar

--- src/pages/Navigation.js
+++ src/pages/Navigation.js
@@ -1,25 +1,51 @@
 import React from 'react';
 import logo from '../eg.svg';
 import { Outlet } from "react-router-dom";
-import { Navbar, Nav } from "react-bootstrap";
+import { Navbar, Nav, Offcanvas, Container } from "react-bootstrap";
 import '../App.css';
 import { LinkContainer } from 'react-router-bootstrap';
 
 const Navigation = () => {
     return (
         <>
-            <Navbar fixed="top" bg="dark" expand="lg">
-                    <Navbar.Brand><img src={logo} alt="logo" className="Nav-logo" /></Navbar.Brand>
-                    <Nav>
+            {['lg'].map((expand) => (
+                <Navbar key={expand} bg="dark" expand={expand}>
+                    <Container fluid>
+                    <img src={logo} alt="logo" className="Nav-logo" />
+                    <Nav className="d-none d-lg-flex">
                         <LinkContainer to="/">
                             <Nav.Link>Home</Nav.Link>    
                         </LinkContainer>
                         <LinkContainer to="/info">
                             <Nav.Link>Info</Nav.Link>                        
-                        </LinkContainer>
-                        
+                        </LinkContainer>    
                     </Nav>
-            </Navbar>
+                        <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
+                        <Navbar.Offcanvas
+                            id={`offcanvasNavbar-expand-${expand}`}
+                            aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
+                            placement="top"
+                            className="Nav-offcanvas"
+                        >
+                            <Offcanvas.Header closeButton>
+                                <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
+                                    Site Links
+                                </Offcanvas.Title>
+                            </Offcanvas.Header>
+                            <Offcanvas.Body>
+                                <Nav>
+                                    <LinkContainer to="/">
+                                        <Nav.Link>Home</Nav.Link>    
+                                    </LinkContainer>
+                                    <LinkContainer to="/info">
+                                        <Nav.Link>Info</Nav.Link>                        
+                                    </LinkContainer>    
+                                </Nav>
+                            </Offcanvas.Body>
+                        </Navbar.Offcanvas>
+                    </Container>
+                </Navbar>
+              ))}
 
         <Outlet />
         </>
